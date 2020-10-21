c4fd91b9ecc481fb830232035063ac34acdd314e
ghost <kai.zhao@karakal.com.cn>
2020-10-21 16:45:55 +0800

    LeetCode刷题  初始化

---
+++ src/main/java/com/ghost/leetcode/array/TwoSum.java
@@ -0,0 +1,15 @@
+package com.ghost.leetcode.array;
+
+/**
+ * @Author NorthernGhost
+ * @Description
+ * @Date 2020/10/21 16:40
+ * @Version 1.0
+ */
+public class TwoSum {
+
+    public static void main(String[] args) {
+
+
+    }
+}
