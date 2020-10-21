01574c88bb9ba23db2ed851590636703d48e735f
FreeBirdArray <z_k1053@163.com>
2020-10-21 20:25:04 +0800

    数组-第268题：missingNumber(消失的数字)

--- src/main/java/com/ghost/leetcode/array/MisssingNumber.java
+++ src/main/java/com/ghost/leetcode/array/MisssingNumber.java
@@ -16,7 +16,13 @@ public class MisssingNumber {
     }
 
 
-
+    /**
+     * 数学法解：通过等差数字前N项和的求和公式sum=(n*(n+1))/2-nums数组的和就能得到确实的数字
+     * 由于对数字求和的时间复杂度为 O(n) 所以此解法的时间复杂度为 O(n)
+     * 由于只是使用了基本数据类型int存储sum所以空间复杂度为 O(n)
+     * @param nums
+     * @return
+     */
     public static int missingNumber(int[] nums){
 
         int sum = (nums.length*(nums.length+1))/2;
